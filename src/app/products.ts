b4de3fce791344c9a7f611ce8257ea7409a231c4
bilyalov01 <aizar_aizar@mail.ru>
2021-02-24 02:10:56 +0600

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,107 @@
+export const products = [
+  {
+    id: 1,
+    image: "https://m.media-amazon.com/images/I/91shKLxoedL._AC_UY218_.jpg",
+    name: "HP Chromebook 14-inch HD Laptop,",
+    description:
+      "Intel Celeron N4000, 4 GB RAM, 32 GB eMMC, Chrome (14a-na0010nr, Mineral Silver)",
+    rating: "4.6 /5.0",
+    link:
+      "https://www.amazon.com/HP-Chromebook-14-inch-Celeron-14a-na0010nr/dp/B08529TZMC/ref=sr_1_1?crid=2GD9704ZSJ6XU&dchild=1&keywords=laptops&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=516a7b4e-d342-442c-96c9-4fbeb114cb90&pf_rd_r=WB96HKHSDX4KE3RCMCVW&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614094885&sprefix=laptops%2Caps%2C236&sr=8-1"
+  },
+  {
+    id: 2,
+    image: "https://m.media-amazon.com/images/I/71r5254QPZL._AC_UY218_.jpg",
+    name: "Razer Blade 15 Base Gaming Laptop 2020",
+    rating: "4.6/5.0",
+    description:
+      " Intel Core i7-10750H 6-Core, NVIDIA GeForce GTX 1660 Ti, 15.6 FHD 1080p 144Hz, 16GB RAM, 256GB SSD, CNC Aluminum, Chroma RGB Lighting, Thunderbolt 3, Black",
+    link:
+      "https://www.amazon.com/Razer-Blade-Base-Gaming-Laptop/dp/B086MGY9TZ/ref=sr_1_3?crid=2GD9704ZSJ6XU&dchild=1&keywords=laptops&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=516a7b4e-d342-442c-96c9-4fbeb114cb90&pf_rd_r=WB96HKHSDX4KE3RCMCVW&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614094885&sprefix=laptops%2Caps%2C236&sr=8-3"
+  },
+  {
+    id: 3,
+    image: "https://m.media-amazon.com/images/I/71LXYdhtD3L._AC_UY218_.jpg",
+    name: "ASUS Laptop L410 Ultra Thin Laptop",
+    rating: "4.3/5.0",
+    description:
+      "14” FHD Display, Intel Celeron N4020 Processor, 4GB RAM, 64GB Storage, NumberPad, Windows 10 Home in S Mode, Star Black, L410MA-DB02",
+    link:
+      "https://www.amazon.com/ASUS-Display-Processor-NumberPad-L410MA-DB02/dp/B08D92T45H/ref=sr_1_5?crid=2GD9704ZSJ6XU&dchild=1&keywords=laptops&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=516a7b4e-d342-442c-96c9-4fbeb114cb90&pf_rd_r=WB96HKHSDX4KE3RCMCVW&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614094885&sprefix=laptops%2Caps%2C236&sr=8-5"
+  },
+  {
+    id: 4,
+    image: "https://m.media-amazon.com/images/I/71vvXGmdKWL._AC_UY218_.jpg",
+    name: "ASUS VivoBook 15 F515 Thin and Light Laptop",
+    rating: "4.5/5.0",
+    description:
+      "15.6” FHD Display, Intel Core i3-1005G1 Processor, 4GB DDR4 RAM, 128GB PCIe SSD, Fingerprint Reader, Windows 10 Home in S Mode, Slate Grey, F515JA-AH31",
+    link:
+      "https://www.amazon.com/ASUS-i3-1005G1-Processor-Fingerprint-F515JA-AH31/dp/B08H4YTTLP/ref=sr_1_6?crid=2GD9704ZSJ6XU&dchild=1&keywords=laptops&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=516a7b4e-d342-442c-96c9-4fbeb114cb90&pf_rd_r=WB96HKHSDX4KE3RCMCVW&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614094885&sprefix=laptops%2Caps%2C236&sr=8-6"
+  },
+  {
+    id: 5,
+    image: "https://m.media-amazon.com/images/I/81Z8NvO2TFL._AC_UY218_.jpg",
+    name: "Acer Nitro 5 Gaming Laptop",
+    rating: "4.6/5.0",
+    description:
+      "0th Gen Intel Core i5-10300H,NVIDIA GeForce GTX 1650 Ti, 15.6 Full HD IPS 144Hz Display, 8GB DDR4,256GB NVMe SSD,WiFi 6, DTS X Ultra,Backlit Keyboard,AN515-55-59KS",
+    link:
+      "https://www.amazon.com/Acer-i5-10300H-GeForce-Keyboard-AN515-55-59KS/dp/B08H2H89K1/ref=sr_1_7?crid=2GD9704ZSJ6XU&dchild=1&keywords=laptops&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=516a7b4e-d342-442c-96c9-4fbeb114cb90&pf_rd_r=WB96HKHSDX4KE3RCMCVW&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614094885&sprefix=laptops%2Caps%2C236&sr=8-7"
+  },
+  {
+    id: 6,
+    image: "https://m.media-amazon.com/images/I/71vFKBpKakL._AC_UY218_.jpg",
+    name: "2020 Apple MacBook Air with Apple M1 Chip",
+    rating: "4.8/5.0",
+    description: "(13-inch, 8GB RAM, 256GB SSD Storage) - Gold",
+    link:
+      "https://www.amazon.com/Apple-MacBook-13-inch-256GB-Storage/dp/B08N5M7S6K/ref=sr_1_8?crid=2GD9704ZSJ6XU&dchild=1&keywords=laptops&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=516a7b4e-d342-442c-96c9-4fbeb114cb90&pf_rd_r=WB96HKHSDX4KE3RCMCVW&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614094885&sprefix=laptops%2Caps%2C236&sr=8-8"
+  },
+  {
+    id: 7,
+    image: "https://m.media-amazon.com/images/I/91MW2X7lrfL._AC_UY218_.jpg",
+    name: "ASUS TUF506IV-AS76 TUF Gaming A15 Gaming Laptop",
+    rating: "4.5/5.0",
+    description:
+      "15.6” 144Hz Full HD IPS-Type, AMD Ryzen 7 4800H, GeForce RTX 2060, 16GB DDR4, 1TB PCIe SSD, 90WHr Battery, RGB Backlit KB, Windows 10 Home",
+    link:
+      "https://www.amazon.com/ASUS-IPS-Type-GeForce-Battery-TUF506IV-AS76/dp/B0863DW238/ref=sr_1_11?crid=2GD9704ZSJ6XU&dchild=1&keywords=laptops&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=516a7b4e-d342-442c-96c9-4fbeb114cb90&pf_rd_r=WB96HKHSDX4KE3RCMCVW&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614094885&sprefix=laptops%2Caps%2C236&sr=8-11"
+  },
+  {
+    id: 8,
+    image: "https://m.media-amazon.com/images/I/71nEy+5OMdL._AC_UY218_.jpg",
+    name: "MSI GE66 Raider 10UG-211",
+    rating: "5.0/5.0",
+    description:
+      " 15.6 300Hz 3ms Gaming Laptop Intel Core i7-10870H RTX3070 32GB 1TB NVMe SSD Win10 VR Ready",
+    link:
+      "https://www.amazon.com/MSI-GE66-Raider-10UG-211-i7-10870H/dp/B08R9FP7RM/ref=sr_1_13?crid=2GD9704ZSJ6XU&dchild=1&keywords=laptops&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=516a7b4e-d342-442c-96c9-4fbeb114cb90&pf_rd_r=WB96HKHSDX4KE3RCMCVW&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614094885&sprefix=laptops%2Caps%2C236&sr=8-13"
+  },
+  {
+    id: 9,
+    image: "https://m.media-amazon.com/images/I/81-7VeKHsSL._AC_UY218_.jpg",
+    name: "2020 HP Touchscreen Laptop Computer",
+    rating: "4.5/5.0",
+    description:
+      " 10th Gen Intel Quard-Core i5 1035G1 up to 3.6GHz/ 12GB DDR4 RAM/ 256GB PCIe SSD/ 802.11ac WiFi/ Bluetooth 4.2/ USB 3.1 Type-C/ HDMI/ Silver/ Windows 10 Home",
+    link:
+      "https://www.amazon.com/HP-Touchscreen-Computer-Quard-Core-Bluetooth/dp/B08BMLZKL2/ref=sr_1_17?crid=2GD9704ZSJ6XU&dchild=1&keywords=laptops&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=516a7b4e-d342-442c-96c9-4fbeb114cb90&pf_rd_r=WB96HKHSDX4KE3RCMCVW&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614094885&sprefix=laptops%2Caps%2C236&sr=8-17"
+  },
+  {
+    id: 10,
+    image: "https://m.media-amazon.com/images/I/71arQOG5qPL._AC_UY218_.jpg",
+    name: "Lenovo IdeaPad 3 Laptop",
+    rating: "4.4/5.0",
+    description:
+      "15.6 HD (1366 x 768) Display, AMD Ryzen 3 3250U Processor, 4GB DDR4 Onboard RAM, 128GB SSD, AMD Radeon Vega 3 Graphics, Windows 10, 81W10094US, Business Black",
+    link:
+      "https://www.amazon.com/Lenovo-Processor-Graphics-81W10094US-Business/dp/B0872K4XCJ/ref=sr_1_18?crid=2GD9704ZSJ6XU&dchild=1&keywords=laptops&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=516a7b4e-d342-442c-96c9-4fbeb114cb90&pf_rd_r=WB96HKHSDX4KE3RCMCVW&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614094885&sprefix=laptops%2Caps%2C236&sr=8-18"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
